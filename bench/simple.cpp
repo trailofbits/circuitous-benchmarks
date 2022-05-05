5876c767438b0cd2bd3c69e0661853045d337087
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-05-05 17:26:25 +0200

    treewide: Setup benchmarking project.

---
+++ bench/simple.cpp
@@ -0,0 +1,12 @@
+#include <benchmark/benchmark.h>
+
+namespace circ::bench {
+    using state_t = benchmark::State;
+
+    static void simple_bench(state_t& state) {
+        for (auto _ : state)
+            std::string empty_string;
+    }
+
+    BENCHMARK(simple_bench);
+}
